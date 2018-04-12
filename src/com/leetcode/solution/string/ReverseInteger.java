ef976a16e73705f4529eb46aa3874ef785e79e83
dadisdad <1030839531@qq.com>
2018-04-12 23:23:10 +0800

    颠倒整数

--- src/com/leetcode/solution/string/ReverseInteger.java
+++ src/com/leetcode/solution/string/ReverseInteger.java
@@ -1,6 +1,9 @@
-package com.leetcode.solution.math;
+package com.leetcode.solution.string;
 
 /**
+ * 给定一个范围为 32 位 int 的整数，将其颠倒。
+ * 假设我们的环境只能处理 32 位 int 范围内的整数。根据这个假设，如果颠倒后的结果超过这个范围，则返回 0。
+ *
  * @author dadisdad
  * @date 2018/1/23
  */
