5de12eb32066b5a937f5f60f38db4d7dfe760adc
dadisdad <1030839531@qq.com>
2018-04-23 17:10:27 +0800

    合并两个有序链表

--- src/com/leetcode/solution/linkedlist/MergeTwoSortedLists.java
+++ src/com/leetcode/solution/linkedlist/MergeTwoSortedLists.java
@@ -62,7 +62,21 @@ public class MergeTwoSortedLists {
     }
 
     public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(4);
+        ListNode n4 = new ListNode(1);
+        ListNode n5 = new ListNode(3);
+        ListNode n6 = new ListNode(4);
 
+        n1.next = n2;
+        n2.next = n3;
+
+        n4.next = n5;
+        n5.next = n6;
+
+        //ListUtils.tranverseListNode(mergeTwoLists1(n1,n4));
+        ListUtils.tranverseListNode(mergeTwoLists2(n1,n4));
     }
 
 }
