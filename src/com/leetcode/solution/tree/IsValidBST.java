65c021b07a8624ed6964c94e9d894805a3e64341
dadisdad <1030839531@qq.com>
2018-04-24 17:40:57 +0800

    验证二叉搜索树

---
+++ src/com/leetcode/solution/tree/IsValidBST.java
@@ -0,0 +1,95 @@
+package com.leetcode.solution.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ * <p>
+ * 一个二叉搜索树具有如下特征：
+ * <p>
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ * <p>
+ * 输入:
+ * 2
+ * / \
+ * 1   3
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入:
+ * 5
+ * / \
+ * 1   4
+ * / \
+ * 3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ * 根节点的值为 5 ，但是其右子节点值为 4 。
+ *
+ * @author dadisdad
+ * @date 2018/4/24
+ */
+public class IsValidBST {
+
+    /**
+     * 初始化时带入系统最大值和最小值，在递归过程中换成它们自己的节点值
+     *
+     * @param root
+     * @return
+     */
+    public static boolean isValidBST1(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public static boolean isValidBST(TreeNode root, long min, long max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
+
+
+    /**
+     * 利用中序遍历
+     *
+     * @param root
+     * @return
+     */
+    public static boolean isValidBST2(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) >= list.get(i + 1)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    private static void inOrder(TreeNode root, List<Integer> list) {
+        if (root != null) {
+            inOrder(root.left, list);
+            list.add(root.val);
+            inOrder(root.right, list);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
