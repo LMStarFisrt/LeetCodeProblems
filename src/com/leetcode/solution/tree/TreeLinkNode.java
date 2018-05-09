820efbe5b2749da3d8b0ba5a7f4463fa3f6d1438
dadisdad <1030839531@qq.com>
2018-05-09 23:13:05 +0800

    每个节点的右向指针

---
+++ src/com/leetcode/solution/tree/TreeLinkNode.java
@@ -0,0 +1,15 @@
+package com.leetcode.solution.tree;
+
+/**
+ * @author dadisdad
+ * @date 2018/5/9
+ */
+public class TreeLinkNode {
+    int val;
+    TreeLinkNode left, right, next;
+
+    TreeLinkNode(int x) {
+        val = x;
+    }
+
+}
