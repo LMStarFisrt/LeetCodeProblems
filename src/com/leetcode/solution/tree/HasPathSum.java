b676a3acaea2c91435daee40b525b2ed43b6aa27
dadisdad <1030839531@qq.com>
2018-06-04 21:52:41 +0800

    路径总和

--- src/com/leetcode/solution/tree/HasPathSum.java
+++ src/com/leetcode/solution/tree/HasPathSum.java
@@ -1,8 +1,55 @@
 package com.leetcode.solution.tree;
 
 /**
+ * 给定一个二叉树和一个目标和，判断该树中是否存在根节点到叶子节点的路径，这条路径上所有节点值相加等于目标和。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ * 给定如下二叉树，以及目标和 sum = 22，
+ *
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \      \
+ *         7    2      1
+ * 返回 true, 因为存在目标和为 22 的根节点到叶子节点的路径 5->4->11->2。
+ *
  * @author dadisdad
  * @date 2018/6/4
  */
 public class HasPathSum {
+
+    public static boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        sum = sum - root.val;
+        if (sum == 0 && root.left==null && root.right == null){
+            return true;
+        }
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(-2);
+        TreeNode n3 = new TreeNode(-3);
+        TreeNode n4 = new TreeNode(1);
+        TreeNode n5 = new TreeNode(3);
+        TreeNode n6 = new TreeNode(-2);
+        TreeNode n7 = new TreeNode(-1);
+
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        n3.left = n6;
+        n4.right = n7;
+
+        System.out.println(hasPathSum(n1, 1));
+    }
 }
