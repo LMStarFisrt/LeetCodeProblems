03d071a8f06d7d6d8f23a20abb3230da61b39545
dadisdad <1030839531@qq.com>
2018-06-18 23:05:54 +0800

    2的幂

--- src/com/leetcode/solution/math/IsPowerOfTwo.java
+++ src/com/leetcode/solution/math/IsPowerOfTwo.java
@@ -24,15 +24,15 @@ package com.leetcode.solution.math;
 public class IsPowerOfTwo {
 
     public static boolean isPowerOfTwo(int n) {
-        if (n<=0){
+        if (n <= 0) {
             return false;
         }
         int tmp = n;
-        while (tmp>1) {
-            if(tmp%2!=0){
+        while (tmp > 1) {
+            if (tmp % 2 != 0) {
                 return false;
             }
-            tmp = tmp/2;
+            tmp = tmp / 2;
         }
         return true;
     }
