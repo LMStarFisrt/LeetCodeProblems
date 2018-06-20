805f7008c213d2fe98ca5fd6fe8971b6f27e6ea8
dadisdad <1030839531@qq.com>
2018-06-20 22:56:11 +0800

    旋转数字

---
+++ src/com/leetcode/solution/string/RotatedDigits.java
@@ -0,0 +1,63 @@
+package com.leetcode.solution.string;
+
+/**
+ * 我们称一个数 X 为好数, 如果它的每位数字逐个地被旋转 180 度后，我们仍可以得到一个有效的，且和 X 不同的数。要求每位数字都要被旋转。
+ * <p>
+ * 如果一个数的每位数字被旋转以后仍然还是一个数字， 则这个数是有效的。0, 1, 和 8 被旋转后仍然是它们自己；2 和 5 可以互相旋转成对方；6 和 9 同理，除了这些以外其他的数字旋转以后都不再是有效的数字。
+ * <p>
+ * 现在我们有一个正整数 N, 计算从 1 到 N 中有多少个数 X 是好数？
+ * <pre>
+ * 示例:
+ * 输入: 10
+ * 输出: 4
+ * 解释:
+ * 在[1, 10]中有四个好数： 2, 5, 6, 9。
+ * 注意 1 和 10 不是好数, 因为他们在旋转之后不变。
+ * 注意:
+ * </pre>
+ * N 的取值范围是 [1, 10000]。
+ *
+ * @author dadisdad
+ * @date 2018/6/20
+ */
+public class RotatedDigits {
+
+    public static int rotatedDigits(int N) {
+        int count = 0;
+        for (int i = 1; i <= N; i++) {
+            String value = String.valueOf(i);
+            if (value.contains("3") || value.contains("4") || value.contains("7")) {
+                continue;
+            }
+            int len = value.length();
+            String tmp = "";
+            for (int j = 0; j < len; j++) {
+                if (value.charAt(j) == '0') {
+                    tmp = tmp.concat("0");
+                } else if (value.charAt(j) == '1') {
+                    tmp = tmp.concat("1");
+                } else if (value.charAt(j) == '2') {
+                    tmp = tmp.concat("5");
+                } else if (value.charAt(j) == '5') {
+                    tmp = tmp.concat("2");
+                } else if (value.charAt(j) == '6') {
+                    tmp = tmp.concat("9");
+                } else if (value.charAt(j) == '8') {
+                    tmp = tmp.concat("8");
+                } else {
+                    tmp = tmp.concat("6");
+                }
+            }
+            if (Integer.parseInt(tmp) == i) {
+                continue;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rotatedDigits(10));
+    }
+}
